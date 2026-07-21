b33b416d5d77ea887cdd1bbf5d7ebbc7dcc4c46d
jayexterkate <jayexterkate@icloud.com>
2026-07-21 09:37:05 +0200

    Autocreated missing packages and added GameState gameobject

---
+++ Assets/GameStateScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameStateScript : MonoBehaviour
+{
+    //state is intended to determine the state of the game, such as "Not started yet", "Started", "Game Over", and whatever else we need.
+    string state = "Start";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
