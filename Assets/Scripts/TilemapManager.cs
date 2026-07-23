9336c5870b61e3e1032ec4cd8a5984fb56b69b7e
Horriblebarry12 <horriblebarry12@gmail.com>
2026-07-22 21:33:44 -0700

    Made a simple script that would loop through the tilemap and find which tile is which, to then do work with later

---
+++ Assets/Scripts/TilemapManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+
+namespace Assets.Scripts
+{
+	public class TilemapManager : MonoBehaviour
+	{
+		Tilemap[] _Tilemaps = new Tilemap[2];
+
+		List<Vector2> WallTiles;
+		List<Vector2> MashineTiles;
+		List<Vector2> OutputTiles;
+
+		// Use this for initialization
+		void Start()
+		{
+			for (int i = 0; i < transform.childCount; i++)
+			{
+				_Tilemaps[i] = transform.GetChild(i).GetComponent<Tilemap>();
+			}
+
+			for (int i = -_Tilemaps[0].size.x; i < _Tilemaps[0].size.x; i++)
+			{
+				for (int j = -_Tilemaps[0].size.y; j < _Tilemaps[0].size.y; j++)
+				{
+					if (_Tilemaps[0].GetTile(new Vector3Int(i, j, 0)) != null)
+						FilterTile(_Tilemaps[0].GetTile(new Vector3Int(i, j, 0)).name, new Vector2(i, j));
+				}
+			}
+			for (int i = -_Tilemaps[1].size.x; i < _Tilemaps[1].size.x; i++)
+			{
+				for (int j = -_Tilemaps[1].size.y; j < _Tilemaps[1].size.y; j++)
+				{
+					if (_Tilemaps[1].GetTile(new Vector3Int(i, j, 0)) != null)
+						FilterTile(_Tilemaps[1].GetTile(new Vector3Int(i, j, 0)).name, new Vector2(i, j));
+				}
+			}
+		}
+
+		void FilterTile(string name, Vector2 pos) 
+		{
+			if (name == "WallSprite") 
+			{
+				WallTiles.Add(pos);
+			}
+			else if (name == "MashineSprite") 
+			{
+				MashineTiles.Add(pos);
+			}
+			else if (name == "DeliveryConveyorSprite") 
+			{
+				OutputTiles.Add(pos);
+			}
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			
+		}
+	}
+}
